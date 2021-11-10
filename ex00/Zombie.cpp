365cb036e55b8a64390c9cc84c46982d0edf9019
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-10 13:36:44 +0900

    fix makefile

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,15 +1,14 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name) {
-  this->name = name;
-  std::cout << this->name << "'s corpse awakened..." << std::endl;
+Zombie::Zombie(std::string name) : _name(name) {
+  std::cout << _name << "'s corpse awakened..." << std::endl;
 }
 
 Zombie::~Zombie() {
-  std::cout << this->name << " doesn't seem to move anymore..." << std::endl;
+  std::cout << _name << " doesn't seem to move anymore..." << std::endl;
 }
 
 void Zombie::announce(void) {
-  std::cout << "<" << this->name << ">"
+  std::cout << "<" << _name << ">"
             << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -1,26 +1,25 @@
 #include "Zombie.hpp"
 
 Zombie::Zombie() {
-  this->name = std::string("Nameless");
-  std::cout << this->name << "'s corpse awakened..." << std::endl;
+  _name = std::string("Nameless");
+  std::cout << _name << "'s corpse awakened..." << std::endl;
 }
 
-Zombie::Zombie(std::string name) {
-  this->name = name;
-  std::cout << this->name << "'s corpse awakened..." << std::endl;
+Zombie::Zombie(std::string name) : _name(name) {
+  std::cout << _name << "'s corpse awakened..." << std::endl;
 }
 
 Zombie::~Zombie() {
-  std::cout << this->name << " doesn't seem to move anymore..." << std::endl;
+  std::cout << _name << " doesn't seem to move anymore..." << std::endl;
 }
 
 void Zombie::set_name(std::string name) {
-  std::cout << this->name;
-  this->name = name;
-  std::cout << " is named " << this->name << "." << std::endl;
+  std::cout << _name;
+  _name = name;
+  std::cout << " is named " << _name << "." << std::endl;
 }
 
 void Zombie::announce(void) {
-  std::cout << "<" << this->name << ">"
+  std::cout << "<" << _name << ">"
             << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
